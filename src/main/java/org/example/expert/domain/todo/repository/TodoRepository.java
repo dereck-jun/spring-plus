96ed575c47ca733dfa9af0aa134a300266ed8d6b
dereck-jun <yjpark0805@naver.com>
2025-03-12 01:55:20 +0900

    Feat: Level 1 완료
    1. 코드 개선 퀴즈 - `@Transactional`의 이해
    2. 코드 추가 퀴즈 - JWT의 이해
    3. 코드 개선 퀴즈 - JPA의 이해
    4. 테스트 코드 퀴즈 - 컨트롤러 테스트의 이해
    5. 코드 개선 퀴즈 - AOP의 이해
    
    Related to: #2

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,5 +1,8 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,15 +10,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
+        "LEFT JOIN t.user " +
+        "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    @Query("select t from Todo t "
+        + "where (:weather = '' or t.weather = :weather) "
+        + "and (:startDateTime is null or :startDateTime <= t.modifiedAt) "
+        + "and (:endDateTime is null or t.modifiedAt <= :endDateTime)")
+    Page<Todo> findAllByWeatherAndModifiedFrom(
+        @Param("weather") String weather,
+        @Param("startDateTime") LocalDateTime startDateTime,
+        @Param("endDateTime") LocalDateTime endDateTime,
+        Pageable pageable
+    );
 }
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,5 +1,8 @@
 package org.example.expert.domain.todo.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import lombok.RequiredArgsConstructor;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -25,58 +28,67 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
         String weather = weatherClient.getTodayWeather();
 
         Todo newTodo = new Todo(
-                todoSaveRequest.getTitle(),
-                todoSaveRequest.getContents(),
-                weather,
-                user
+            todoSaveRequest.getTitle(),
+            todoSaveRequest.getContents(),
+            weather,
+            user
         );
         Todo savedTodo = todoRepository.save(newTodo);
 
         return new TodoSaveResponse(
-                savedTodo.getId(),
-                savedTodo.getTitle(),
-                savedTodo.getContents(),
-                weather,
-                new UserResponse(user.getId(), user.getEmail())
+            savedTodo.getId(),
+            savedTodo.getTitle(),
+            savedTodo.getContents(),
+            weather,
+            new UserResponse(user.getId(), user.getEmail())
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    public Page<TodoResponse> getTodos(
+        String weather,
+        LocalDate startDate,
+        LocalDate endDate,
+        int page,
+        int size
+    ) {
         Pageable pageable = PageRequest.of(page - 1, size);
+        LocalDateTime startDateTime = startDate != null ? startDate.atStartOfDay() : null;
+        LocalDateTime endDateTime = endDate != null ? endDate.atTime(23, 59, 59) : null;
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findAllByWeatherAndModifiedFrom(weather, startDateTime, endDateTime, pageable);
 
         return todos.map(todo -> new TodoResponse(
-                todo.getId(),
-                todo.getTitle(),
-                todo.getContents(),
-                todo.getWeather(),
-                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
-                todo.getCreatedAt(),
-                todo.getModifiedAt()
+            todo.getId(),
+            todo.getTitle(),
+            todo.getContents(),
+            todo.getWeather(),
+            new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
+            todo.getCreatedAt(),
+            todo.getModifiedAt()
         ));
     }
 
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
-                .orElseThrow(() -> new InvalidRequestException("Todo not found"));
+            .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
         User user = todo.getUser();
 
         return new TodoResponse(
-                todo.getId(),
-                todo.getTitle(),
-                todo.getContents(),
-                todo.getWeather(),
-                new UserResponse(user.getId(), user.getEmail()),
-                todo.getCreatedAt(),
-                todo.getModifiedAt()
+            todo.getId(),
+            todo.getTitle(),
+            todo.getContents(),
+            todo.getWeather(),
+            new UserResponse(user.getId(), user.getEmail()),
+            todo.getCreatedAt(),
+            todo.getModifiedAt()
         );
     }
 }
