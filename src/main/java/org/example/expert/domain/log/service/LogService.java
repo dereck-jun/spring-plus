70dcc3d3f73a0d99407db50abc41e22aea5f0f6c
dereck-jun <yjpark0805@naver.com>
2025-03-13 17:18:40 +0900

    Feat: Level 3 - 11
    11. Transaction 심화
    
    Related to: #6

---
+++ src/main/java/org/example/expert/domain/log/service/LogService.java
@@ -0,0 +1,31 @@
+package org.example.expert.domain.log.service;
+
+import org.example.expert.domain.log.entity.Log;
+import org.example.expert.domain.log.repository.LogRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+
+	private final LogRepository logRepository;
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void recordRequestLog(String traceId, Long userId, String httpMethod, String requestUri) {
+		logRepository.save(new Log(traceId, userId, httpMethod, requestUri));
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void recordSuccessfulLog(String traceId, Long userId, String httpMethod, String requestUri, String httpStatus) {
+		logRepository.save(new Log(traceId, userId, httpMethod, requestUri, httpStatus));
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void recordFailedLog(String traceId, Long userId, String httpMethod, String requestUri, String httpStatus, String errorMessage) {
+		logRepository.save(new Log(traceId, userId, httpMethod, requestUri, httpStatus, errorMessage));
+	}
+}
